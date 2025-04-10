cd546e1512c2982de863f80ab587871671c38d23
jhernandez02 <jhernandez02@gmail.com>
2025-04-10 18:29:37 -0500

    update server.js

--- Tema10 - Websocket y Docker/Clase48_07-04-2025/chat-sockets/server.js
+++ Tema10 - Websocket y Docker/Clase48_07-04-2025/chat-sockets/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('public'));
 
 const server = app.listen(3000, ()=>{
-    console.log("Servidor iniciado en el puerto 3000");
+    console.log("Servidor iniciado en el puerto: 3000");
 });
 
 const io = socket(server);
